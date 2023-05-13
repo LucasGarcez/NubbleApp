7edb9f3a237f73ed826d09d318ea8404e030ad6c
Lucas Garcez <lucasgarcezdev@gmail.com>
2023-05-13 16:54:59 +1000

    Text Component: fontSize and lineHeight

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  Text as RNText,
+  TextProps as RNTextProps,
+  TextStyle,
+} from 'react-native';
+
+interface TextProps extends RNTextProps {
+  preset?: TextVariants;
+}
+export function Text({
+  children,
+  preset = 'paragraphMedium',
+  style,
+  ...rest
+}: TextProps) {
+  return (
+    <RNText style={[$fontSizes[preset], style]} {...rest}>
+      {children}
+    </RNText>
+  );
+}
+
+type TextVariants =
+  | 'headingLarge'
+  | 'headingMedium'
+  | 'headingSmall'
+  | 'paragraphLarge'
+  | 'paragraphMedium'
+  | 'paragraphSmall'
+  | 'paragraphCaption'
+  | 'paragraphCaptionSmall';
+
+const $fontSizes: Record<TextVariants, TextStyle> = {
+  headingLarge: {fontSize: 32, lineHeight: 38.4},
+  headingMedium: {fontSize: 22, lineHeight: 26.4},
+  headingSmall: {fontSize: 18, lineHeight: 23.4},
+
+  paragraphLarge: {fontSize: 18, lineHeight: 25.2},
+  paragraphMedium: {fontSize: 16, lineHeight: 22.4},
+  paragraphSmall: {fontSize: 14, lineHeight: 19.6},
+
+  paragraphCaption: {fontSize: 12, lineHeight: 16.8},
+  paragraphCaptionSmall: {fontSize: 10, lineHeight: 14},
+};
