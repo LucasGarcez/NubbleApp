8cb56522fc5c0ffa52793768106287442ab64b7a
Lucas Garcez <lucasgarcezdev@gmail.com>
2023-06-22 22:32:44 +1000

    update images URL

--- src/domain/Post/postListMock.ts
+++ src/domain/Post/postListMock.ts
@@ -5,13 +5,11 @@ export const postListMock: Post[] = [
     id: 'post_1',
     text: 'This is my first post!',
     author: {
-      profileURL:
-        'https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+      profileURL: 'https://i.imgur.com/YeE3yKf.jpg',
       name: 'John Doe',
       userName: 'johndoe',
     },
-    imageURL:
-      'https://images.unsplash.com/photo-1501504905252-473c47e087f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+    imageURL: 'https://i.imgur.com/VyfuWYa.jpg',
     reactionCount: 10,
     commentCount: 5,
     favoriteCount: 2,
@@ -20,13 +18,11 @@ export const postListMock: Post[] = [
     id: 'post_2',
     text: 'Check out this cool photo!',
     author: {
-      profileURL:
-        'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+      profileURL: 'https://i.imgur.com/BrJUnRl.jpg',
       name: 'Jane Smith',
       userName: 'janesmith',
     },
-    imageURL:
-      'https://images.unsplash.com/photo-1491438590914-bc09fcaaf77a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    imageURL: 'https://i.imgur.com/KkcrB5y.jpg',
     reactionCount: 15,
     commentCount: 8,
     favoriteCount: 3,
@@ -35,13 +31,11 @@ export const postListMock: Post[] = [
     id: 'post_3',
     text: "Just finished reading this book and it's amazing!",
     author: {
-      profileURL:
-        'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+      profileURL: 'https://i.imgur.com/BxgHDci.jpg',
       name: 'Mark Johnson',
       userName: 'markjohnson',
     },
-    imageURL:
-      'https://images.unsplash.com/photo-1506880018603-83d5b814b5a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+    imageURL: 'https://i.imgur.com/NdrA2BY.jpg',
     reactionCount: 20,
     commentCount: 12,
     favoriteCount: 5,
@@ -50,13 +44,11 @@ export const postListMock: Post[] = [
     id: 'post_4',
     text: "I can't believe it's already May!",
     author: {
-      profileURL:
-        'https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+      profileURL: 'https://i.imgur.com/FmSbPv3.jpg',
       name: 'Amy Lee',
       userName: 'amylee',
     },
-    imageURL:
-      'https://images.unsplash.com/photo-1556764900-61987c92731d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+    imageURL: 'https://i.imgur.com/vkscdqk.jpg',
     reactionCount: 5,
     commentCount: 3,
     favoriteCount: 1,
@@ -65,13 +57,11 @@ export const postListMock: Post[] = [
     id: 'post_5',
     text: 'Coding day!',
     author: {
-      profileURL:
-        'https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=689&q=80',
+      profileURL: 'https://i.imgur.com/DF4Jfxq.jpg',
       name: 'Santiago Emilio',
       userName: 'sanemilio',
     },
-    imageURL:
-      'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80',
+    imageURL: 'https://i.imgur.com/rsOe2hC.jpg',
     reactionCount: 0,
     commentCount: 0,
     favoriteCount: 0,
@@ -80,13 +70,11 @@ export const postListMock: Post[] = [
     id: 'post_6',
     text: "What's your favorite color?",
     author: {
-      profileURL:
-        'https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+      profileURL: 'https://i.imgur.com/YeE3yKf.jpg',
       name: 'John Doe',
       userName: 'johndoe',
     },
-    imageURL:
-      'https://images.unsplash.com/photo-1491900177661-4e1cd2d7cce2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    imageURL: 'https://i.imgur.com/zx94i19.jpg',
     reactionCount: 2,
     commentCount: 1,
     favoriteCount: 0,
@@ -95,13 +83,11 @@ export const postListMock: Post[] = [
     id: 'post_7',
     text: "I'm so excited for the weekend!",
     author: {
-      profileURL:
-        'https://images.unsplash.com/photo-1598550880863-4e8aa3d0edb4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80',
+      profileURL: 'https://i.imgur.com/BrJUnRl.jpg',
       name: 'Jane Smith',
       userName: 'janesmith',
     },
-    imageURL:
-      'https://images.unsplash.com/photo-1541532713592-79a0317b6b77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80',
+    imageURL: 'https://i.imgur.com/LWopzZH.jpg',
     reactionCount: 8,
     commentCount: 4,
     favoriteCount: 1,
@@ -110,13 +96,11 @@ export const postListMock: Post[] = [
     id: 'post_8',
     text: 'Just got back from vacation and it was amazing!',
     author: {
-      profileURL:
-        'https://images.unsplash.com/photo-1544435253-f0ead49638fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+      profileURL: 'https://i.imgur.com/BxgHDci.jpg',
       name: 'Mark Johnson',
       userName: 'markjohnson',
     },
-    imageURL:
-      'https://plus.unsplash.com/premium_photo-1681266166596-f4c1c78375a3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    imageURL: 'https://i.imgur.com/WZZLeHg.jpg',
     reactionCount: 25,
     commentCount: 10,
     favoriteCount: 4,
@@ -124,11 +108,9 @@ export const postListMock: Post[] = [
   {
     id: 'post_9',
     text: "I'm loving this new restaurant!",
-    imageURL:
-      'https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+    imageURL: 'https://i.imgur.com/FmSbPv3.jpg',
     author: {
-      profileURL:
-        'https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+      profileURL: 'https://i.imgur.com/OGQRFok.jpg',
       name: 'Amy Lee',
       userName: 'amylee',
     },
