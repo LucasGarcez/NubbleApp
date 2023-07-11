b4dbb6124a245141dcf8695a29da65b1ac134f97
Lucas Garcez <lucasgarcezdev@gmail.com>
2023-07-12 02:04:34 +1000

    7 - Aplicando o Adapter

---
+++ src/domain/Post/postAdapter.ts
@@ -0,0 +1,22 @@
+import {Post, PostAPI} from './postTypes';
+
+/**
+ * @description Adapta o PostAPI para o modelo de Post
+ */
+function toPost(postAPI: PostAPI): Post {
+  return {
+    id: postAPI.id.toString(),
+    text: postAPI.text,
+    author: {
+      profileURL: postAPI.user.profile_url,
+      name: postAPI.user.full_name,
+      userName: postAPI.user.username,
+    },
+    imageURL: postAPI.image_url,
+    reactionCount: parseInt(postAPI.meta.like_count, 10),
+    commentCount: parseInt(postAPI.meta.comments_count, 10),
+    favoriteCount: parseInt(postAPI.meta.favorite_count, 10),
+  };
+}
+
+export const postAdapter = {toPost};
--- src/domain/Post/postService.ts
+++ src/domain/Post/postService.ts
@@ -1,9 +1,11 @@
+import {postAdapter} from './postAdapter';
 import {postApi} from './postApi';
 import {Post} from './postTypes';
 
 async function getList(): Promise<Post[]> {
-  const postList = await postApi.getList();
-  return postList;
+  const postPageAPI = await postApi.getList();
+
+  return postPageAPI.data.map(postAdapter.toPost);
 }
 
 export const postService = {
