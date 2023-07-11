4b520af2c0c7f5c9e77098b149aacdaeb11756f8
Lucas Garcez <lucasgarcezdev@gmail.com>
2023-07-11 21:55:21 +1000

    4 - integração fetch API

--- src/domain/Post/postApi.ts
+++ src/domain/Post/postApi.ts
@@ -2,8 +2,18 @@ import {postListMock} from './postListMock';
 import {Post} from './types';
 
 async function getList(): Promise<Post[]> {
-  //TODO: simular um delay na API
-  await new Promise(resolve => setTimeout(() => resolve(''), 1000));
+  let response = await fetch('http://localhost:3333/user/post', {
+    method: 'GET',
+    headers: {
+      Authorization:
+        'Bearer Mg.R5wy9_TgoQXaBCRrhnJhuOKaDZhvFqFnhSrnI0xYsOMHJfLC6lj45lCaC-7d',
+    },
+  });
+
+  let data = await response.json();
+  console.log('FETCH DATA:', data);
+
+  // await new Promise(resolve => setTimeout(() => resolve(''), 1000));
   return postListMock;
 }
 
