bd3f4b2b9ae3fc3620fa128e12142d3cd2a78b2d
Lucas Garcez <lucasgarcezdev@gmail.com>
2023-07-13 02:32:24 +1000

    14 - scroll to top

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -8,6 +8,7 @@ import {
 } from 'react-native';
 
 import {Post, usePostList} from '@domain';
+import {useScrollToTop} from '@react-navigation/native';
 
 import {PostItem, Screen} from '@components';
 import {AppTabScreenProps} from '@routes';
@@ -18,6 +19,9 @@ import {HomeHeader} from './components/HomeHeader';
 export function HomeScreen({}: AppTabScreenProps<'HomeScreen'>) {
   const {postList, error, loading, refresh, fetchNextPage} = usePostList();
 
+  const flatListRef = React.useRef<FlatList<Post>>(null);
+  useScrollToTop(flatListRef);
+
   function renderItem({item}: ListRenderItemInfo<Post>) {
     return <PostItem post={item} />;
   }
@@ -25,6 +29,7 @@ export function HomeScreen({}: AppTabScreenProps<'HomeScreen'>) {
   return (
     <Screen style={$screen}>
       <FlatList
+        ref={flatListRef}
         showsVerticalScrollIndicator={false}
         data={postList}
         keyExtractor={item => item.id}
