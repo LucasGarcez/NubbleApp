a4e0d1770d166c902042f3878e271158c26687ae
Lucas Garcez <lucasgarcezdev@gmail.com>
2023-08-05 04:00:29 +1000

    16 - usePostCommentRemove

--- src/domain/PostComment/index.ts
+++ src/domain/PostComment/index.ts
@@ -3,3 +3,4 @@ export * from './postCommentService';
 
 export * from './useCases/usePostCommentList';
 export * from './useCases/usePostCommentCreate';
+export * from './useCases/usePostCommentRemove';
--- src/screens/app/PostCommentScreen/components/PostCommentItem.tsx
+++ src/screens/app/PostCommentScreen/components/PostCommentItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import {Alert, Pressable} from 'react-native';
 
-import {PostComment} from '@domain';
+import {PostComment, usePostCommentRemove} from '@domain';
 
 import {Box, ProfileAvatar, Text} from '@components';
 
@@ -8,17 +9,34 @@ interface Props {
   postComment: PostComment;
 }
 export function PostCommentItem({postComment}: Props) {
+  const {mutate} = usePostCommentRemove();
+
+  function confirmRemove() {
+    Alert.alert('Deseja excluir o comentário?', 'pressione confirmar', [
+      {
+        text: 'Confirmar',
+        onPress: () => mutate({postCommentId: postComment.id}),
+      },
+      {
+        text: 'Cancelar',
+        style: 'cancel',
+      },
+    ]);
+  }
+
   return (
-    <Box flexDirection="row" alignItems="center" mb="s16">
-      <ProfileAvatar imageURL={postComment.author.profileURL} />
-      <Box ml="s12" flex={1}>
-        <Text preset="paragraphSmall" bold>
-          {postComment.author.userName}
-        </Text>
-        <Text preset="paragraphSmall" color="gray1">
-          {postComment.message} - {postComment.createdAtRelative}
-        </Text>
+    <Pressable onLongPress={confirmRemove}>
+      <Box flexDirection="row" alignItems="center" mb="s16">
+        <ProfileAvatar imageURL={postComment.author.profileURL} />
+        <Box ml="s12" flex={1}>
+          <Text preset="paragraphSmall" bold>
+            {postComment.author.userName}
+          </Text>
+          <Text preset="paragraphSmall" color="gray1">
+            {postComment.message} - {postComment.createdAtRelative}
+          </Text>
+        </Box>
       </Box>
-    </Box>
+    </Pressable>
   );
 }
