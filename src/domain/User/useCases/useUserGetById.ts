f4daf643a9ff48bd3c6cb5a0cc4e6024cd47eed8
Lucas Garcez <lucasgarcezdev@gmail.com>
2023-08-14 00:25:26 +1000

    3 - staleTIme and cacheTime

--- src/domain/User/useCases/useUserGetById.ts
+++ src/domain/User/useCases/useUserGetById.ts
@@ -1,36 +1,15 @@
-import {useCallback, useEffect, useState} from 'react';
-
 import {QueryKeys} from '@infra';
 import {useQuery} from '@tanstack/react-query';
 
 import {userService} from '../userService';
-import {User} from '../userTypes';
 
 export function useUserGetById(id: number) {
-  // const [user, setUser] = useState<User>();
-  // const [error, setError] = useState<boolean | null>(null);
-  // const [loading, setLoading] = useState(false);
-
   const {data, isLoading, isError} = useQuery({
     queryKey: [QueryKeys.UserGetById, id],
     queryFn: () => userService.getById(id),
+    staleTime: 1000 * 30, // 10 seconds
+    // cacheTime: 5000,
   });
-
-  // const getUserById = useCallback(async () => {
-  //   try {
-  //     setLoading(true);
-  //     const _user = await userService.getById(id);
-  //     setUser(_user);
-  //   } catch (er) {
-  //     setError(true);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // }, [id]);
-
-  // useEffect(() => {
-  //   getUserById();
-  // }, [getUserById]);
 
   return {
     user: data,
