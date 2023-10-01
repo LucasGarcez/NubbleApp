1186d837f25ded671e2ba358e49a970c704faffa
Lucas Garcez <lucasgarcezdev@gmail.com>
2023-10-01 22:25:01 +1100

    api updates

--- src/domain/Auth/authAdapter.ts
+++ src/domain/Auth/authAdapter.ts
@@ -7,6 +7,8 @@ function toAuthCredentials(
 ): AuthCredentials {
   return {
     token: authCredentialsAPI.auth.token,
+    tokenExpiresAt: authCredentialsAPI.auth.expires_at,
+    refreshToken: authCredentialsAPI.auth.refreshToken,
     user: userAdapter.toUser(authCredentialsAPI.user),
   };
 }
