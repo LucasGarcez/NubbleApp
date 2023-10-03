a45c11a34db94db588e3f62fc3305b16f163bbf1
Lucas Garcez <lucasgarcezdev@gmail.com>
2023-10-03 19:57:28 +1100

    1 - update authAPI (backend-v4)

--- src/domain/Auth/authApi.ts
+++ src/domain/Auth/authApi.ts
@@ -13,7 +13,7 @@ async function signIn(
   email: string,
   password: string,
 ): Promise<AuthCredentialsAPI> {
-  const response = await api.post<AuthCredentialsAPI>('login', {
+  const response = await api.post<AuthCredentialsAPI>('auth/login', {
     email,
     password,
   });
@@ -21,21 +21,24 @@ async function signIn(
 }
 
 async function signOut(): Promise<string> {
-  const response = await api.get<string>('profile/logout');
+  const response = await api.get<string>('auth/profile/logout');
   return response.data;
 }
 
 async function signUp(data: SignUpDataAPI): Promise<UserAPI> {
-  const response = await api.post<UserAPI>('register', data);
+  const response = await api.post<UserAPI>('auth/register', data);
   return response.data;
 }
 
 async function isUserNameAvailable(params: {
   username: string;
 }): Promise<FieldIsAvailableAPI> {
-  const response = await api.get<FieldIsAvailableAPI>('validate-username', {
-    params,
-  });
+  const response = await api.get<FieldIsAvailableAPI>(
+    'auth/validate-username',
+    {
+      params,
+    },
+  );
 
   return response.data;
 }
@@ -43,7 +46,7 @@ async function isUserNameAvailable(params: {
 async function isEmailAvailable(params: {
   email: string;
 }): Promise<FieldIsAvailableAPI> {
-  const response = await api.get<FieldIsAvailableAPI>('validate-email', {
+  const response = await api.get<FieldIsAvailableAPI>('auth/validate-email', {
     params,
   });
 
@@ -53,7 +56,10 @@ async function isEmailAvailable(params: {
 async function forgotPassword(
   params: ForgotPasswordParam,
 ): Promise<{message: string}> {
-  const response = await api.post<{message: string}>('forgot-password', params);
+  const response = await api.post<{message: string}>(
+    'auth/forgot-password',
+    params,
+  );
 
   return response.data;
 }
