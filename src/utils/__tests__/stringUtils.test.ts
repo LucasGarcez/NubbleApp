5c426a5bf2c513730fb9cc36ec81a1adaa8d5ef0
Lucas Garcez <lucasgarcezdev@gmail.com>
2023-10-08 22:34:44 +1100

    3 - group and describe

--- src/utils/__tests__/stringUtils.test.ts
+++ src/utils/__tests__/stringUtils.test.ts
@@ -1,11 +1,18 @@
 import {stringUtils} from '@utils';
-test('capitalizeFirstLetter', () => {
-  // escrever meu teste
-  stringUtils.capitalizeFirstLetter('Ana maria'); // Ana Maria
-  stringUtils.capitalizeFirstLetter('ANA MARIA'); // Ana Maria
-  stringUtils.capitalizeFirstLetter('MaRIA'); // Maria
 
-  const nome = stringUtils.capitalizeFirstLetter('Ana eduarda');
+describe('stringUtils', () => {
+  describe('capitalizeFirstLetter', () => {
+    it('should capitalize the first letter of each word', () => {
+      expect(stringUtils.capitalizeFirstLetter('Ana maria')).toBe('Ana Maria');
+      expect(stringUtils.capitalizeFirstLetter('ANA MARIA')).toBe('Ana Maria');
+      expect(stringUtils.capitalizeFirstLetter('maria')).toBe('Maria');
+      expect(stringUtils.capitalizeFirstLetter('MARIA')).toBe('Maria');
+      expect(stringUtils.capitalizeFirstLetter('ana maria')).toBe('Ana Maria');
+    });
 
-  expect(nome).toBe('Ana Eduarda');
+    it('should remove leading/trailing spaces', () => {
+      expect(stringUtils.capitalizeFirstLetter(' Ana maria')).toBe('Ana Maria');
+      expect(stringUtils.capitalizeFirstLetter('Ana maria ')).toBe('Ana Maria');
+    });
+  });
 });
