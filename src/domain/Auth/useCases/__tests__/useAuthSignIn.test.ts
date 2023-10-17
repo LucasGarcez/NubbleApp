8bdebce999ad018d29ede6fbd8f192048775743d
Lucas Garcez <lucasgarcezdev@gmail.com>
2023-10-17 21:26:34 +1100

    4 - test saveCredentials

--- src/domain/Auth/useCases/__tests__/useAuthSignIn.test.ts
+++ src/domain/Auth/useCases/__tests__/useAuthSignIn.test.ts
@@ -5,6 +5,19 @@ import {authService} from '../../authService';
 import {useAuthSignIn} from '../useAuthSignIn';
 
 import {mockedAuthCredentials} from './mockedData/mocks';
+
+const mockedSaveCredentials = jest.fn();
+
+jest.mock('@services', () => {
+  const originalModule = jest.requireActual('@services');
+  return {
+    ...originalModule,
+    useAuthCredentials: () => ({
+      saveCredentials: mockedSaveCredentials,
+    }),
+  };
+});
+
 describe('useAuthSignIn', () => {
   it('saves credentials if the sign-in successfully', async () => {
     jest
@@ -18,6 +31,8 @@ describe('useAuthSignIn', () => {
     result.current.signIn({email: 'lucas@coffstack.com', password: '123'});
 
     await waitFor(() => expect(result.current.isSuccess).toBe(true));
+
+    expect(mockedSaveCredentials).toHaveBeenCalledWith(mockedAuthCredentials);
   });
 
   it('calls the onError function with a message if sign-in fails', () => {
