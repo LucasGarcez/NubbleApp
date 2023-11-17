55bad03d2b766861a7c958a8321dba1909a6c524
Lucas Garcez <lucasgarcezdev@gmail.com>
2023-11-17 20:34:59 +1100

    4 - ProfileUser

--- src/components/ProfileUser/ProfileUser.tsx
+++ src/components/ProfileUser/ProfileUser.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import {Pressable} from 'react-native';
 
-import {Post} from '@domain';
+import {User} from '@domain';
 import {useNavigation} from '@react-navigation/native';
 
 import {Box, Text, ProfileAvatar} from '@components';
 
-type Props = Pick<Post, 'author'>;
-export function PostHeader({author}: Props) {
+type ProfileUserProps = {user: Pick<User, 'username' | 'profileUrl' | 'id'>};
+export function ProfileUser({user}: ProfileUserProps) {
   const navigation = useNavigation();
 
   function navigateTpProfile() {
-    navigation.navigate('ProfileScreen', {userId: author.id});
+    navigation.navigate('ProfileScreen', {userId: user.id});
   }
 
   return (
     <Pressable onPress={navigateTpProfile}>
       <Box flexDirection="row" alignItems="center" mb="s16">
-        <ProfileAvatar imageURL={author.profileURL} />
+        <ProfileAvatar imageURL={user.profileUrl} />
         <Text ml="s12" semiBold preset="paragraphMedium">
-          {author.userName}
+          {user.username}
         </Text>
       </Box>
     </Pressable>
--- src/screens/app/SearchScreen/SearchScreen.tsx
+++ src/screens/app/SearchScreen/SearchScreen.tsx
@@ -1,8 +1,9 @@
 import React, {useState} from 'react';
+import {FlatList, ListRenderItemInfo} from 'react-native';
 
-import {useUserSearch} from '@domain';
+import {User, useUserSearch} from '@domain';
 
-import {Icon, Screen, Text, TextInput} from '@components';
+import {Icon, ProfileUser, Screen, TextInput} from '@components';
 import {useDebounce} from '@hooks';
 import {AppScreenProps} from '@routes';
 
@@ -11,6 +12,10 @@ export function SearchScreen({}: AppScreenProps<'SearchScreen'>) {
   const debouncedSearch = useDebounce(search);
 
   const {list} = useUserSearch(debouncedSearch);
+
+  function renderItem({item}: ListRenderItemInfo<User>) {
+    return <ProfileUser user={item} />;
+  }
 
   return (
     <Screen
@@ -23,9 +28,11 @@ export function SearchScreen({}: AppScreenProps<'SearchScreen'>) {
           LeftComponent={<Icon color="gray3" name="search" />}
         />
       }>
-      {list.map(user => (
-        <Text key={user.id}>{user.username}</Text>
-      ))}
+      <FlatList
+        data={list}
+        renderItem={renderItem}
+        keyExtractor={item => item.username}
+      />
     </Screen>
   );
 }
