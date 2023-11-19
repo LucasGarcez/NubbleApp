60de1e4053089147927833ba362d847a12467012
Lucas Garcez <lucasgarcezdev@gmail.com>
2023-11-19 22:07:19 +1100

    14 - IT fakeTimers and next steps

--- src/screens/app/SearchScreen/__tests__/integration/SearchScreen.test.tsx
+++ src/screens/app/SearchScreen/__tests__/integration/SearchScreen.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {authCredentialsStorage} from '@services';
 import {mockUtils, server, userMocked} from '@test';
-import {fireEvent, renderScreen, screen} from 'test-utils';
+import {act, fireEvent, renderScreen, screen} from 'test-utils';
 
 import {AppStack} from '@routes';
 
@@ -10,6 +10,7 @@ jest.unmock('@react-navigation/native');
 
 beforeAll(() => {
   server.listen();
+  jest.useFakeTimers();
   jest
     .spyOn(authCredentialsStorage, 'get')
     .mockResolvedValue(mockUtils.mateusAuthCredentials);
@@ -21,25 +22,46 @@ afterEach(() => {
 
 afterAll(() => {
   server.close();
+  jest.useRealTimers();
   jest.resetAllMocks();
 });
 
 describe('integration: SearchScreen', () => {
   test('Search Flow', async () => {
+    // 1) Navigate to Search Screen
     renderScreen(<AppStack initialRouteName="SearchScreen" />);
 
+    // 2) Find the search input and type user name
     const inputText = screen.getByPlaceholderText(/digite sua busca/i);
     fireEvent.changeText(inputText, 'mar');
+    act(() => jest.runAllTimers());
 
+    // 3) Find two users as per the MSW mock
     const user1 = await screen.findByText(userMocked.user1.username);
     expect(user1).toBeTruthy();
 
     const user2 = await screen.findByText(userMocked.user2.username);
     expect(user2).toBeTruthy();
 
+    // 4) Select the user1 and navigate to Profile Screen
     fireEvent.press(user1);
 
+    // 5) Expect to be at the Profile Screen with the user1 loaded
     const userFullName = await screen.findByText(userMocked.user1.full_name);
     expect(userFullName).toBeTruthy();
+
+    // 6) Press the back button to navigate back to Search Screen
+
+    // 7) Clean the search input
+
+    // 8) Make sure the search history (buscas recentes) section appears
+
+    // 9) The user1 (pressed) was the saved in the search history
+
+    // 10) The user2 (NOT pressed) must NOT appear in the search history
+
+    // 11) Remove user1 from the search history by pressing the trash icon
+
+    // 12) Make sure the user1 was removed from the search history
   });
 });
