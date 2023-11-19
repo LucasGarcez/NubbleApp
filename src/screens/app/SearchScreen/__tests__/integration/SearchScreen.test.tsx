cb3ec248a6d6b2bec9dfc54de96bf5257d3468e1
Lucas Garcez <lucasgarcezdev@gmail.com>
2023-11-19 18:06:08 +1100

    fix typo

--- src/screens/app/SearchScreen/__tests__/integration/SearchScreen.test.tsx
+++ src/screens/app/SearchScreen/__tests__/integration/SearchScreen.test.tsx
@@ -23,7 +23,7 @@ afterAll(() => {
 
 describe('integration: SearchScreen', () => {
   test('Test Search User: find, select, persist search history, and remove from history', async () => {
-    // 1) Render AppStack with SearchScreen as initial screen
+    // 1) Navigate to Search Screen - Render AppStack with Search Screen as the initial screen
     renderScreen(<AppStack initialRouteName="SearchScreen" />);
 
     // 2) Find the search text input and type "mar"
@@ -34,41 +34,41 @@ describe('integration: SearchScreen', () => {
     // const debouncedSearch = useDebounce(search, 2000);
     act(() => jest.runAllTimers()); // skip text input debounce
 
-    // 3) Find two user as per MSW mock
+    // 3) Find two users as per the MSW mock
     const user1 = await screen.findByText(userMocked.user1.username);
     expect(user1).toBeTruthy();
 
     const user2 = await screen.findByText(userMocked.user2.username);
     expect(user2).toBeTruthy();
 
-    // 4) Select the first use to navigate to his profile
+    // 4) Select the user1 to navigate to their profile
     fireEvent.press(user1);
 
-    // 5) expect to be at the profile screen with the user 1 loaded
+    // 5) Expect to be at the profile screen with the user 1 loaded
     const userName = await screen.findByText(userMocked.user1.full_name);
     expect(userName).toBeTruthy();
 
-    // 6) press the back button to navigate back to search
+    // 6) Press the back button to navigate back to search
     const backButton = screen.getByTestId('screen-back-button');
     fireEvent.press(backButton);
 
-    // 7) clean the search text input
+    // 7) Clean the search text input
     fireEvent.changeText(inputText, '');
     act(() => jest.runAllTimers()); // skip text input debounce
 
-    // 8) Make sure the search history screen show up by looking for "buscas recentes"
+    // 8) Make sure the search history screen appears by searching for "recent searches"
     const searchListTitle = screen.getByText(/buscas recentes/i);
     expect(searchListTitle).toBeTruthy();
 
-    // 9) The user1 pressed in the search was persisted and appears in search history
+    // 9) The user1 (pressed) was saved in the search history
     const user1Again = screen.queryByText(userMocked.user1.username);
     expect(user1Again).toBeTruthy();
 
-    // 10) The user2 NOT pressed in the search must NOT appears on the search history
+    // 10) The user2 (NOT pressed) must NOT appear in the search history
     const user2Again = screen.queryByText(userMocked.user2.username);
     expect(user2Again).toBeFalsy();
 
-    // 11) Remove user1 from search history by pressing trash icon
+    // 11) Remove user1 from the search history by pressing the trash icon
     const trashIcon = screen.getByTestId('trash');
     fireEvent.press(trashIcon);
 
