ea395f08edf044c4a01b7c301691d02c8fc8340a
Lucas Garcez <lucasgarcezdev@gmail.com>
2024-02-02 11:57:26 +1100

    21 - React Native VisionCamera

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,4 @@ export * from './useAppSafeArea';
 export * from './useAppTheme';
 export * from './useResetNavigationSuccess';
 export * from './useDebounce';
+export * from './useAppState';
--- src/screens/app/CameraScreen/CameraScreen.tsx
+++ src/screens/app/CameraScreen/CameraScreen.tsx
@@ -1,8 +1,11 @@
 import React, {useState} from 'react';
 import {Dimensions, StyleSheet} from 'react-native';
 
+import {useIsFocused} from '@react-navigation/native';
+import {Camera, useCameraDevice} from 'react-native-vision-camera';
+
 import {Box, BoxProps, Icon, PermissionManager} from '@components';
-import {useAppSafeArea} from '@hooks';
+import {useAppSafeArea, useAppState} from '@hooks';
 import {AppScreenProps} from '@routes';
 
 const CAMERA_VIEW = Dimensions.get('screen').width;
@@ -12,6 +15,13 @@ const CONTROL_DIFF = 30;
 export function CameraScreen({navigation}: AppScreenProps<'CameraScreen'>) {
   const {top} = useAppSafeArea();
   const [flashOn, setFlashOne] = useState(false);
+  const device = useCameraDevice('back');
+
+  const isFocused = useIsFocused();
+  const appState = useAppState();
+  const isActive = isFocused && appState === 'active';
+
+  console.log({isFocused, appState, isActive});
 
   function toggleFlash() {
     setFlashOne(prev => !prev);
@@ -21,7 +31,13 @@ export function CameraScreen({navigation}: AppScreenProps<'CameraScreen'>) {
       permissionName="camera"
       description="Permita o Nubble acessar a camera">
       <Box flex={1}>
-        <Box backgroundColor="grayWhite" style={StyleSheet.absoluteFill} />
+        {device != null && (
+          <Camera
+            style={StyleSheet.absoluteFill}
+            device={device}
+            isActive={isActive}
+          />
+        )}
 
         <Box flex={1} justifyContent="space-between">
           <Box {...$controlAreaTop} style={{paddingTop: top}}>
