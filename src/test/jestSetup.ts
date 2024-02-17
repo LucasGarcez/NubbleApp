bd15b44f40d4bf9621d270fa2ed5631820e9cd77
Lucas Garcez <lucasgarcezdev@gmail.com>
2024-02-17 20:34:13 +1100

    add expo-image-manipulator

--- src/test/jestSetup.ts
+++ src/test/jestSetup.ts
@@ -42,4 +42,8 @@ jest.mock('../services/permission/permissionService', () => ({
   },
 }));
 
+jest.mock('expo-image-manipulator', () => ({
+  manipulateAsync: jest.fn(),
+}));
+
 initializeStorage(inMemoryStorage);
