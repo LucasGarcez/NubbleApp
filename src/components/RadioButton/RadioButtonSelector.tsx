07c7e2e1fdc3c300901e97e00350f2e4c4d65ead
Lucas Garcez <lucasgarcezdev@gmail.com>
2024-03-06 09:00:38 +1100

    8 - Generic RadioButton

--- src/components/RadioButton/RadioButtonSelector.tsx
+++ src/components/RadioButton/RadioButtonSelector.tsx
@@ -3,14 +3,37 @@ import React from 'react';
 import {Box} from '../Box/Box';
 import {Separator} from '../Separator/Separator';
 
-import {RadioButtonItem, RadioButtonItemProps} from './RadioButtonItem';
+import {RadioButtonItem} from './RadioButtonItem';
 
-export function RadioButtonSelector({items}: {items: RadioButtonItemProps[]}) {
+type ItemTConstraint = Record<string, any>;
+export type RadioButtonSelectorProps<ItemT extends ItemTConstraint> = {
+  items: ItemT[];
+  selectedItem?: ItemT;
+  onSelect: (item: ItemT) => void;
+  labelKey: keyof ItemT;
+  descriptionKey: keyof ItemT;
+  valueKey: keyof ItemT;
+};
+export function RadioButtonSelector<ItemT extends ItemTConstraint>({
+  items,
+  selectedItem,
+  onSelect,
+  labelKey,
+  descriptionKey,
+  valueKey,
+}: RadioButtonSelectorProps<ItemT>) {
   return (
     <Box>
       {items.map((item, index) => (
         <Box key={item.label}>
-          <RadioButtonItem {...item} />
+          <RadioButtonItem
+            label={item[labelKey]}
+            description={item[descriptionKey]}
+            onPress={() => onSelect(item)}
+            isSelected={
+              !!selectedItem && selectedItem[valueKey] === item[valueKey]
+            }
+          />
           {index < items.length - 1 && <Separator />}
         </Box>
       ))}
--- src/screens/app/DarkModeScreen/DarkModeScreen.tsx
+++ src/screens/app/DarkModeScreen/DarkModeScreen.tsx
@@ -1,32 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {RadioButtonSelector, Screen} from '@components';
+import {RadioButtonSelector, Screen, Text} from '@components';
 import {AppScreenProps} from '@routes';
 
-const items = [
+type ThemePreference = 'light' | 'dark' | 'system';
+
+type Option = {
+  label: string;
+  description?: string;
+  themePreference: ThemePreference;
+};
+
+const items: Option[] = [
   {
     label: 'Ativado',
-    onPress: () => {},
-    isSelected: false,
+    themePreference: 'dark',
   },
   {
     label: 'Desativado',
-    isSelected: true,
-    onPress: () => {},
+    themePreference: 'light',
   },
   {
     label: 'Padrão do sistema',
+    themePreference: 'system',
     description:
       'A aparência será a mesma que você configurou no seu dispositivo',
-    onPress: () => {},
-    isSelected: false,
   },
 ];
 
 export function DarkModeScreen({}: AppScreenProps<'DarkModeScreen'>) {
+  const [selectedItem, setSelectedItem] = useState<Option>();
   return (
     <Screen canGoBack title="Modo escuro">
-      <RadioButtonSelector items={items} />
+      <RadioButtonSelector
+        items={items}
+        selectedItem={selectedItem}
+        onSelect={setSelectedItem}
+        labelKey="label"
+        valueKey="themePreference"
+        descriptionKey="description"
+      />
+
+      <Text preset="paragraphLarge" mt="s48">
+        {JSON.stringify(selectedItem, null, 2)}
+      </Text>
     </Screen>
   );
 }
