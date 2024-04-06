3537ff47f52ceb5b02d62f2af6813f180261ff52
Lucas Garcez <lucasgarcezdev@gmail.com>
2024-04-06 19:58:50 +1100

    8 - expo module android

--- android/app/src/main/java/com/nubbleapp/MainActivity.kt
+++ android/app/src/main/java/com/nubbleapp/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.nubbleapp;
+import expo.modules.ReactActivityDelegateWrapper
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -23,6 +24,6 @@ class MainActivity : ReactActivity() {
      * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
      */
     override fun createReactActivityDelegate(): ReactActivityDelegate =
-            DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+            ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED, DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled))
 }
 
