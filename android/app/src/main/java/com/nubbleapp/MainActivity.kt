b85edfb7d74ad40996064c5eaa6abdeb365cf820
Lucas Garcez <lucasgarcezdev@gmail.com>
2024-04-06 19:00:09 +1100

    7 - fix build, screen e app name

--- android/app/src/main/java/com/nubbleapp/MainActivity.kt
+++ android/app/src/main/java/com/nubbleapp/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : ReactActivity() {
      * Returns the name of the main component registered from JavaScript. This is used to schedule
      * rendering of the component.
      */
-    override fun getMainComponentName(): String = "RnDiffApp"
+    override fun getMainComponentName(): String = "NubbleApp"
 
     /**
      * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
