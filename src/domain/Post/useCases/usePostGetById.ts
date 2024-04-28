9fc75a7e467451c5fe5b2bffdb3e0f926680bee2
Lucas Garcez <lucasgarcezdev@gmail.com>
2024-04-29 06:45:38 +1000

    undo

--- src/domain/Post/useCases/usePostGetById.ts
+++ src/domain/Post/useCases/usePostGetById.ts
@@ -5,7 +5,7 @@ import {postService} from '../postService';
 
 export function usePostGetById(id: number, enabled: boolean) {
   const {data, isLoading, isError, refetch, isFetching} = useQuery({
-    queryKey: [QueryKeys.PostGetById, id],
+    queryKey: [QueryKeys.UserGetById, id],
     queryFn: () => postService.getById(id),
     staleTime: 1000 * 30, // 30 seconds
     enabled,
