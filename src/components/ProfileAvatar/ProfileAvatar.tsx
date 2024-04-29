527a794d817cb9a72ae5af8ca7144c5deda6f486
Lucas Garcez <lucasgarcezdev@gmail.com>
2024-04-30 06:12:49 +1000

    PostAvatar and PostCommentItem

--- src/components/ProfileAvatar/ProfileAvatar.tsx
+++ src/components/ProfileAvatar/ProfileAvatar.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Image} from 'react-native';
+import {Image, Pressable} from 'react-native';
+
+import {useNavigateToProfile} from '@hooks';
 
 export interface ProfileAvatarProps {
   imageURL: string;
@@ -7,16 +9,28 @@ export interface ProfileAvatarProps {
   size?: number;
   /** @default 14 */
   borderRadius?: number;
+  /** if available, it will allow navigate to the author profile */
+  authorId?: number;
 }
 export function ProfileAvatar({
   imageURL,
   size = 32,
   borderRadius = 14,
+  authorId,
 }: ProfileAvatarProps) {
+  const navigateToProfile = useNavigateToProfile();
+
+  function handlePress() {
+    if (authorId) {
+      navigateToProfile(authorId);
+    }
+  }
   return (
-    <Image
-      source={{uri: imageURL}}
-      style={{width: size, height: size, borderRadius}}
-    />
+    <Pressable disabled={!authorId} onPress={handlePress}>
+      <Image
+        source={{uri: imageURL}}
+        style={{width: size, height: size, borderRadius}}
+      />
+    </Pressable>
   );
 }
--- src/screens/app/PostCommentScreen/components/PostCommentItem.tsx
+++ src/screens/app/PostCommentScreen/components/PostCommentItem.tsx
@@ -54,7 +54,10 @@ export function PostCommentItem({
         flexDirection="row"
         alignItems="center"
         mb="s16">
-        <ProfileAvatar imageURL={postComment.author.profileUrl} />
+        <ProfileAvatar
+          authorId={postComment.author.id}
+          imageURL={postComment.author.profileUrl}
+        />
         <Box ml="s12" flex={1}>
           <Text preset="paragraphSmall" bold>
             {postComment.author.userName}
