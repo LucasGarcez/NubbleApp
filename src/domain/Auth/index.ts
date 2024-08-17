af7b9119ed0996094a48d5b9c5942ee3e8544dab
Lucas Garcez <lucasgarcezdev@gmail.com>
2024-08-18 07:22:58 +1000

    17 - form async

--- src/domain/Auth/index.ts
+++ src/domain/Auth/index.ts
@@ -1,7 +1,6 @@
 export * from './useCases/useAuthSignIn';
 export * from './useCases/useAuthSignOut';
 export * from './useCases/useAuthSignUp';
-export * from './useCases/useAuthIsValueAvailable';
 export * from './useCases/useAuthRequestNewPassword';
 export * from './authTypes';
 export * from './authService';
--- src/form/index.ts
+++ src/form/index.ts
@@ -1,1 +1,2 @@
 export * from './schemaTypes';
+export * from './useAsyncValidation';
