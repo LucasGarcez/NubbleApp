58beb5acd3a504ffa50846b32b6774d2ee5018df
Lucas Garcez <lucasgarcezdev@gmail.com>
2024-09-21 20:55:52 +1000

    26 - errorUtils getErrorMessage

--- src/domain/Auth/useCases/useAuthUpdatePassword.ts
+++ src/domain/Auth/useCases/useAuthUpdatePassword.ts
@@ -1,5 +1,6 @@
 import {MutationOptions} from '@infra';
 import {useMutation} from '@tanstack/react-query';
+import {errorUtils} from '@utils';
 
 import {EditPasswordParams} from '..';
 import {authService} from '../authService';
@@ -8,6 +9,11 @@ export function useAuthUpdatePassword(options?: MutationOptions<string>) {
   const {mutate, isLoading} = useMutation<string, unknown, EditPasswordParams>({
     mutationFn: params => authService.updatePassword(params),
     retry: false,
+    onError: error => {
+      if (options?.onError) {
+        options?.onError(errorUtils.getErrorMessage(error));
+      }
+    },
     onSuccess: message => {
       if (options?.onSuccess) {
         options?.onSuccess(message);
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './dateUtils';
 export * from './stringsUtils';
+export * from './errorUtils';
