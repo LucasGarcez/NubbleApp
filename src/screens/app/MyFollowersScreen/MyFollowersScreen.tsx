6bcd87d61158c534049dba13741d32ae48062f3e
Lucas Garcez <lucasgarcezdev@gmail.com>
2024-12-02 22:32:02 +1100

    5 - UserListTemplate component

--- src/screens/app/MyFollowersScreen/MyFollowersScreen.tsx
+++ src/screens/app/MyFollowersScreen/MyFollowersScreen.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
-import {Screen, Text} from '@components';
+import {followService} from '@domain';
+import {QueryKeys} from '@infra';
+
+import {UserListTemplate} from '@components';
 import {AppScreenProps} from '@routes';
 
 export function MyFollowersScreen({}: AppScreenProps<'MyFollowersScreen'>) {
   return (
-    <Screen flex={1} title="Seguidores">
-      <Text>Seguidores</Text>
-    </Screen>
+    <UserListTemplate
+      screenTitle="Seguidores"
+      emptyMessage="Você ainda não está seguindo ninguém"
+      totalText="seguindo"
+      queryKey={QueryKeys.MyFollowersList}
+      getUserList={followService.getMyFollowersList}
+      button={{
+        title: 'Remover',
+        onPress: followUser => console.log({followUser}),
+      }}
+    />
   );
 }
--- src/screens/app/MyFollowingScreen/MyFollowingScreen.tsx
+++ src/screens/app/MyFollowingScreen/MyFollowingScreen.tsx
@@ -1,63 +1,23 @@
-import React, {useState} from 'react';
-import {ListRenderItemInfo} from 'react-native';
+import React from 'react';
 
-import {FollowUser, followService} from '@domain';
+import {followService} from '@domain';
 import {QueryKeys} from '@infra';
 
-import {
-  Button,
-  InfinityScrollList,
-  ProfileUser,
-  Screen,
-  Text,
-} from '@components';
+import {UserListTemplate} from '@components';
 import {AppScreenProps} from '@routes';
 
 export function MyFollowingScreen({}: AppScreenProps<'MyFollowingScreen'>) {
-  const [totalUser, setTotalUser] = useState<number | null>(null);
-  function renderItem({item}: ListRenderItemInfo<FollowUser>) {
-    return (
-      <ProfileUser
-        user={item}
-        RightComponent={
-          <Button
-            title="Seguindo"
-            onPress={() => console.log('remove')}
-            preset="gray"
-          />
-        }
-      />
-    );
-  }
-
-  function renderListHeader() {
-    if (!totalUser) {
-      return null;
-    }
-
-    return (
-      <Text semiBold preset="paragraphSmall" color="primary" mb="s24">
-        {totalUser} seguindo
-      </Text>
-    );
-  }
-
-  async function getList(page: number) {
-    const response = await followService.geMyFollowingList(page);
-    setTotalUser(response.meta.total);
-    return response;
-  }
-
   return (
-    <Screen flex={1} title="Seguindo" canGoBack>
-      <InfinityScrollList
-        renderItem={renderItem}
-        getList={getList}
-        queryKey={[QueryKeys.MyFollowingList]}
-        flatListProps={{
-          ListHeaderComponent: renderListHeader,
-        }}
-      />
-    </Screen>
+    <UserListTemplate
+      screenTitle="Seguindo"
+      emptyMessage="Você ainda não está seguindo ninguém"
+      totalText="seguindo"
+      queryKey={QueryKeys.MyFollowingList}
+      getUserList={followService.geMyFollowingList}
+      button={{
+        title: 'Seguindo',
+        onPress: followUser => console.log({followUser}),
+      }}
+    />
   );
 }
