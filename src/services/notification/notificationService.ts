9976b1855deafcbc173625bda3eb9b05994cdc65
Lucas Garcez <lucasgarcezdev@gmail.com>
2024-12-26 13:41:17 +1100

    15 - getInitialNotification and parse data

--- src/services/notification/notificationService.ts
+++ src/services/notification/notificationService.ts
@@ -1,8 +1,34 @@
 import messaging from '@react-native-firebase/messaging';
 
+import {NotificationToNavigate} from './notificationTypes';
+
 async function getToken(): Promise<string> {
   const token = await messaging().getToken();
   return token;
 }
 
-export const notificationService = {getToken};
+async function getInitialNotification(): Promise<NotificationToNavigate | null> {
+  const notification = await messaging().getInitialNotification();
+  if (notification?.data) {
+    return getActionFromNotificationData(notification.data);
+  }
+  return null;
+}
+
+function getActionFromNotificationData(data: {
+  [key: string]: string | object;
+}): NotificationToNavigate | null {
+  if (typeof data.navigate === 'string') {
+    const navigateProps = JSON.parse(data.navigate);
+    if (typeof navigateProps.screen === 'string') {
+      const screen = navigateProps.screen;
+      const params = navigateProps.params || undefined;
+
+      return {screen, params};
+    }
+  }
+
+  return null;
+}
+
+export const notificationService = {getToken, getInitialNotification};
